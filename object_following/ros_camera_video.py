35924814d384266f80861f2525876a01e46cd12a
Bhavana Jain <bmjain2@illinois.edu>
2020-12-12 23:44:11 -0600

    Adds code for tracking objects in the ROS camera feed

--- object_following/ros_camera_video.py
+++ object_following/ros_camera_video.py
@@ -1,57 +1,102 @@
 import os, sys
-from os.path import dirname, abspath
+lib_path = "/home/bhavana*/.local/lib/python2.7/site-packages"
+if os.path.exists(lib_path):
+    sys.path.insert(0, lib_path)
 
-likely_lib_path = os.path.join(
-    dirname(dirname(dirname(abspath(__file__)))), "lib", "p27_site_packages"
-)
-if os.path.exists(likely_lib_path):
-    sys.path.insert(0, likely_lib_path)
-
-import roslib
-import rospy
-from std_msgs.msg import Bool
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
-import time
-import cv2
-from imutils import paths
-import numpy as np
+import argparse
+from imutils.video import FPS
+from imutils.video import VideoStream
 import imutils
+import numpy as np
+from imutils import paths
+import cv2
+import time
+from cv_bridge import CvBridge, CvBridgeError
+from sensor_msgs.msg import Image
+from std_msgs.msg import Bool
+import rospy
+import roslib
 
-# Define the codec and create VideoWriter object
-frame_width = 640
-frame_height = 480
-fourcc = cv2.VideoWriter_fourcc(*"XVID")
-video_writer = cv2.VideoWriter("output.avi", fourcc, 20, (frame_width, frame_height))
 
-class image_converter:
-    def __init__(self):
+class object_tracker:
+    def __init__(self, tracker_type):
         self.bridge = CvBridge()
+        # subscribe to the camera feed
         self.image_sub = rospy.Subscriber(
             "/mako_1/mako_1/image_raw", Image, self.callback
         )
+        self.tracker_type = tracker_type
+
+        # define tracker object based on cv version and tracker type
+        (major, minor) = cv2.__version__.split(".")[:2]
+        if int(major) == 3 and int(minor) < 3:
+            self.tracker = cv2.Tracker_create(tracker_type.upper())
+        else:
+            OPENCV_OBJECT_TRACKERS = {
+                "csrt": cv2.TrackerCSRT_create,
+                "kcf": cv2.TrackerKCF_create,
+                "boosting": cv2.TrackerBoosting_create,
+                "mil": cv2.TrackerMIL_create,
+                "tld": cv2.TrackerTLD_create,
+                "medianflow": cv2.TrackerMedianFlow_create,
+                "mosse": cv2.TrackerMOSSE_create
+            }
+            self.tracker = OPENCV_OBJECT_TRACKERS[tracker_type]()
+        self.initBB = None
 
     def callback(self, data):
         try:
-            global video_writer
             cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-            video_writer.write(cv_image)
+            frame = imutils.resize(cv_image, width=500)
+            (H, W) = frame.shape[:2]
+
+            if self.initBB is not None:
+                (success, box) = self.tracker.update(frame)
+                if success:
+                    (x, y, w, h) = [int(v) for v in box]
+                    cv2.rectangle(frame, (x, y), (x + w, y + h),
+                                  (0, 255, 0), 2)
+                self.fps.update()
+                self.fps.stop()
+                info = [
+                    ("Tracker", self.tracker_type),
+                    ("Success", "Yes" if success else "No"),
+                    ("FPS", "{:.2f}".format(self.fps.fps())),
+                ]
+                for (i, (k, v)) in enumerate(info):
+                    text = "{}: {}".format(k, v)
+                    cv2.putText(frame, text, (10, H - ((i * 20) + 20)),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+
+            cv2.imshow("ROS Camera Feed", frame)
+            key = cv2.waitKey(1) & 0xFF
+
+            # if the 's' key is selected, we are going to "select" a bounding
+            # box to track
+            if key == ord("s"):
+                self.initBB = cv2.selectROI("ROS Camera Feed", frame, fromCenter=False,
+                                            showCrosshair=True)
+                self.tracker.init(frame, self.initBB)
+                self.fps = FPS().start()
+
+            # if the `q` key was pressed, break from the loop
+            elif key == ord("q"):
+                sys.exit(0)
 
         except CvBridgeError as e:
-            global video_writer
-            video_writer.release()
-            cv2.destroyAllWindows()
             print(e)
 
 
-rospy.init_node("image_converter", anonymous=True)
-ic = image_converter()
+parser = argparse.ArgumentParser()
+parser.add_argument("-t", "--tracker", type=str, default="kcf",
+                help="OpenCV object tracker type")
+args = vars(parser.parse_args())
+
+rospy.init_node("object_tracker", anonymous=True)
+ic = object_tracker(args["tracker"])
 
 try:
     rospy.spin()
 except KeyboardInterrupt:
-    global video_writer
-    video_writer.release()
     cv2.destroyAllWindows()
-
     print("Shutting down")
